a5842cc68b50e6347160366845cc5e802e82da14
unknown <jparks@MALJPARKS.corp.flp>
2015-09-22 10:36:03 -0400

    Modified to reflect transfer to Frontline

--- NJsonApi/Properties/AssemblyInfo.cs
+++ NJsonApi/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NJsonApi")]
+[assembly: AssemblyTitle("Util-JsonApiSerializer")]
 [assembly: AssemblyDescription(".NET implementation of the {json:api} compliant sever serializer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SocialCee")]
-[assembly: AssemblyProduct("NJsonApi")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("FrontLine Technologies")]
+[assembly: AssemblyProduct("Util-JsonApiSerializer")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
